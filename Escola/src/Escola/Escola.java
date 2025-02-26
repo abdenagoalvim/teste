22a9f97027be8c505814ab40b7f7a2a4775b309d
Prof. Abdenago Alvim <116508758+abdenagoalvim@users.noreply.github.com>
2025-02-26 16:30:35 -0300

    Add files via upload

---
+++ Escola/src/Escola/Escola.java
@@ -0,0 +1,10 @@
+package Escola;
+
+public class Escola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Escola/src/Escola/Pessoa.java
@@ -0,0 +1,6 @@
+package Escola;
+
+public class Pessoa {
+	private String Nome;
+	
+}
